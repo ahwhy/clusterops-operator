08a222018e3e2e019b9e72de2cf04a7c028ca1ef
ahwhya <ahwhya@outlook.com>
2023-09-13 21:49:24 +0800

    add rbac for 'make manifests'

--- internal/controller/application_controller.go
+++ internal/controller/application_controller.go
@@ -47,6 +47,11 @@ type ApplicationReconciler struct {
 //+kubebuilder:rbac:groups=apps.clusterops.io,resources=applications/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=apps.clusterops.io,resources=applications/finalizers,verbs=update
 
+//+kubebuilder:rbac:groups=apps,resources=deployment,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=apps,resources=deployment/status,verbs=get
+//+kubebuilder:rbac:groups=core,resources=service,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=core,resources=service/status,verbs=get
+
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
 // TODO(user): Modify the Reconcile function to compare the state specified by
