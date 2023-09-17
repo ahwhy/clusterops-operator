b560b041564c12e70b5a2b687f395e040c4358c7
ahwhya <ahwhya@outlook.com>
2023-09-17 13:46:58 +0800

    rejust manager rbac

--- internal/controller/application_controller.go
+++ internal/controller/application_controller.go
@@ -53,10 +53,10 @@ type ApplicationReconciler struct {
 //+kubebuilder:rbac:groups=apps.clusterops.io,resources=applications/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=apps.clusterops.io,resources=applications/finalizers,verbs=update
 
-//+kubebuilder:rbac:groups=apps,resources=deployment,verbs=get;list;watch;create;update;patch;delete
-//+kubebuilder:rbac:groups=apps,resources=deployment/status,verbs=get
-//+kubebuilder:rbac:groups=core,resources=service,verbs=get;list;watch;create;update;patch;delete
-//+kubebuilder:rbac:groups=core,resources=service/status,verbs=get
+//+kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=apps,resources=deployments/status,verbs=get
+//+kubebuilder:rbac:groups=core,resources=services,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=core,resources=services/status,verbs=get
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
